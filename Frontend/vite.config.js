588b0cd36dc92ba8a641ba876367c1aa2c0d1e68
Tech-YashaswiniN <yashawininanjappajigithub@gmail.com>
2024-11-03 16:58:02 +0530

    Food-Delivery-React-Project

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this matches your settings in Render
+  },
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
